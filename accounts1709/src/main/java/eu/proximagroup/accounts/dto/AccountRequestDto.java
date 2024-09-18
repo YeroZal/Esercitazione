2a4e405416f58bce7cd23254a9e1e8854cd3861c
LeonardoFagnani-dev <leofagnani.00@gmail.com>
2024-09-18 15:35:20 +0200

    account dto update

---
+++ accounts1709/src/main/java/eu/proximagroup/accounts/dto/AccountRequestDto.java
@@ -0,0 +1,23 @@
+package eu.proximagroup.accounts.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccountRequestDto {
+
+	@NotNull
+	private Long accountNumber;
+	
+    @NotEmpty(message = "account_type is required")
+    private String accountType;
+	
+    @NotEmpty(message = "branch_address is required")
+    private String branchAddress;
+	
+    @NotNull(message = "customer_id is required")
+    private Long customerId;
+    
+}
