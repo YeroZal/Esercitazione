d2e5229556ae91a923cd8d6e3161f0013ca5e02f
LeonardoFagnani-dev <leofagnani.00@gmail.com>
2024-09-18 15:36:23 +0200

    account repository update

---
+++ accounts1709/src/main/java/eu/proximagroup/accounts/repositories/CustomerRepository.java
@@ -0,0 +1,16 @@
+package eu.proximagroup.accounts.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import eu.proximagroup.accounts.entities.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	Optional<Customer> findByEmailOrMobileNumber(String email, String mobileNumber);
+	
+	List<Customer> findBySex(String sex);
+
+}
