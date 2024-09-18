7d16b1ab6b29d140c4e9f9cfa7177df2c2ab1e67
LeonardoFagnani-dev <leofagnani.00@gmail.com>
2024-09-18 15:43:38 +0200

    loan constants update

---
+++ loans/src/main/java/eu/proximagroup/loans/costants/LoanCostants.java
@@ -0,0 +1,14 @@
+package eu.proximagroup.loans.costants;
+
+public class LoanCostants {
+	
+	public static final String ERROR_ID_NUMERIC = "Invalid ID: must be a numeric value.";
+
+	public static final String MESSAGE_404 = "Loan not found";
+	public static final String MESSAGE_200 = "Success";
+	public static final String MESSAGE_201 = "Created successfully";
+	public static final String MESSAGE_204 = "Deleted successfully";
+	
+	public static final String MESSAGE_ALREADY_PHONE = "mobile number already in use";
+
+}
