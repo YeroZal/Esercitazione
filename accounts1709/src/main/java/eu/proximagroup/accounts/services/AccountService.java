ecb6a000950015f1520136a5a0d86d8158f6b7b4
LeonardoFagnani-dev <leofagnani.00@gmail.com>
2024-09-18 15:36:42 +0200

    account service update

---
+++ accounts1709/src/main/java/eu/proximagroup/accounts/services/AccountService.java
@@ -0,0 +1,50 @@
+package eu.proximagroup.accounts.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import eu.proximagroup.accounts.entities.Account;
+import eu.proximagroup.accounts.repositories.AccountRepository;
+
+@Service
+public class AccountService {
+
+	private AccountRepository accountRepository;
+
+	public AccountService(AccountRepository accountRepository) {
+		this.accountRepository = accountRepository;
+	}
+
+	public List<Account> getAll() {
+		return this.accountRepository.findAll();
+	}
+
+	public Optional<Account> getById(Long id) {
+		return this.accountRepository.findById(id);
+	}
+
+	public Account store(Account account) {
+
+		if (account.getAccountNumber() == null) {
+
+			account.setAccountNumber(0L);
+			int randomNumero = (int) (Math.random() * 201);
+			account.setAccountNumber((long) randomNumero);
+		}
+
+		return this.accountRepository.save(account);
+
+	}
+
+	public Account update(Account account, Long id) {
+		account.setId(id);
+		return this.accountRepository.save(account);
+	}
+
+	public void deleteById(Long id) {
+		this.accountRepository.deleteById(id);
+	}
+
+}
---
+++ accounts1709/src/main/java/eu/proximagroup/accounts/services/CustomerService.java
@@ -0,0 +1,57 @@
+package eu.proximagroup.accounts.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import eu.proximagroup.accounts.entities.Customer;
+import eu.proximagroup.accounts.exceptions.ResourceNotFoundException;
+import eu.proximagroup.accounts.repositories.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+	private CustomerRepository customerRepository;
+
+	public CustomerService(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	public List<Customer> getAll() {
+		return this.customerRepository.findAll();
+	}
+
+	public List<Customer> getSex(String sex) {
+		return this.customerRepository.findBySex(sex);
+	}
+
+	public Customer getById(Long id) {
+		Customer customer = this.customerRepository.findById(id)
+				.orElseThrow( () -> new ResourceNotFoundException("Customer", "Id", id.toString()));
+		
+		return customer;
+		
+	}
+
+	public Customer store(Customer customer) {
+		return this.customerRepository.save(customer);
+	}
+
+	public Customer update(Customer customer, Long id) {
+		customer.setId(id);
+		return this.customerRepository.save(customer);
+	}
+
+	public void deleteById(Long id) {
+		this.customerRepository.findById(id)
+				.orElseThrow( () -> new ResourceNotFoundException("Customer", "Id", id.toString()));
+		
+		this.customerRepository.deleteById(id);
+	}
+
+	public Optional<Customer> getByEmailOrMobileNumber(String email, String mobileNumber) {
+		return this.customerRepository.findByEmailOrMobileNumber(email, mobileNumber);
+	}
+
+}
