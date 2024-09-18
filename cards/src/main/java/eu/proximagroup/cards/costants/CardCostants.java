e485fd1c06883e15f1950bbe6e6c2466c2275cda
LeonardoFagnani-dev <leofagnani.00@gmail.com>
2024-09-18 15:39:24 +0200

    card constants update

---
+++ cards/src/main/java/eu/proximagroup/cards/costants/CardCostants.java
@@ -0,0 +1,14 @@
+package eu.proximagroup.cards.costants;
+
+public class CardCostants {
+	
+	public static final String ERROR_ID_NUMERIC = "Invalid ID: must be a numeric value.";
+
+	public static final String MESSAGE_404 = "Loan not found";
+	public static final String MESSAGE_200 = "Success";
+	public static final String MESSAGE_201 = "Created successfully";
+	public static final String MESSAGE_204 = "Deleted successfully";
+	
+	public static final String MESSAGE_ALREADY_PHONE_CARD = "mobile number and card number already in use";
+
+}
