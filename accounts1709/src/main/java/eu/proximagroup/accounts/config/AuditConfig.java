7efbe3347649a5eedbeedf598e97bcb67802b5cf
CristianTroia <cristian.troia2001@gmail.com>
2024-09-19 15:03:04 +0200

    auditConfig edit

--- accounts1709/src/main/java/eu/proximagroup/accounts/config/AuditConfig.java
+++ accounts1709/src/main/java/eu/proximagroup/accounts/config/AuditConfig.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.AuditorAware;
 
 @Configuration
 public class AuditConfig {
-	
+	 
 	@Bean
 	public AuditorAware<String> auditorAware(){
 		return new AuditorAwareImpl();
